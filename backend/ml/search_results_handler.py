af5bcad48027fbabf05e02fde374cbbf4960e6cf
Pomni <amazing@digital.circus>
2024-04-07 02:34:45 +0000

    feat: proper continouts qna

--- backend/ml/search_results_handler.py
+++ backend/ml/search_results_handler.py
@@ -5,7 +5,7 @@ from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, System
 from schemas.event import Event, EventType
 
 system_prompt_template = load_prompt("ml/prompts/search_system.yaml")
-
+next_system_prompt_template = load_prompt("ml/prompts/search_qna_system.yaml")
 
 def generate_metadata_string(event_type: EventType, metadata: dict[str, Any]) -> str:
     if event_type == EventType.RESTAURANT:
@@ -44,6 +44,16 @@ def generate_messages_for_chat(user_input: str, event: Event) -> list[BaseMessag
     metadata_string = generate_metadata_string(event.type_, event.export_metadata())
     system_message = system_prompt_template.format(
         user_input_text=user_input,
+        event_name=event.name,
+        event_description=event.description,
+        event_metadata=metadata_string,
+    )
+    messages = [SystemMessage(content=system_message)]
+    return messages
+
+def generate_messages_for_continous_chat(event: Event) -> list[BaseMessage]:
+    metadata_string = generate_metadata_string(event.type_, event.export_metadata())
+    system_message = next_system_prompt_template.format(
         event_name=event.name,
         event_description=event.description,
         event_metadata=metadata_string,
